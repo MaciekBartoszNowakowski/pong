93106c29dd8124be474b37cc6298284f426a5d59
MaciekBartoszNowakowski <macieknowakowski556@gmail.com>
2023-03-10 20:50:56 +0100

    Pong, w którym pozbyłem się problemu z uzależneniem prędkości objetów od liczby fps

--- main.py
+++ main.py
@@ -14,61 +14,81 @@ box1=pygame.Rect(10,10,10,80)
 box2=pygame.Rect(1260,10,10,80)
 ball=pygame.Rect(637,357,6,6)
 
-rpion=1
-rpoz=1
-op=0
-speed=60
+b_spin=1
+rpion=b_spin
+rpoz=b_spin
+max_tps=120
+odb=0
+dt=0.0
+
+clock=pygame.time.Clock()
+delta = 0.0
 
 while True:
     #Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
+
+    # TICKING
+    delta += clock.tick()/1000.0
+
+    while delta > 1/max_tps:
+
     # INPUT
-    if pygame.key.get_pressed() [pygame.K_s]:
-        if box1.y + box1.height < rez[1]:
-            box1.y +=1
-    if pygame.key.get_pressed() [pygame.K_w]:
-        if box1.y >0:
-            box1.y -= 1
+        if pygame.key.get_pressed() [pygame.K_s]:
+            if box1.y + box1.height < rez[1]:
+                box1.y += 4
+        if pygame.key.get_pressed() [pygame.K_w]:
+            if box1.y >0:
+                box1.y -= 4
 
-    if pygame.key.get_pressed() [pygame.K_DOWN]:
-        if box2.y + box2.height < rez[1]:
-            box2.y +=1
-    if pygame.key.get_pressed() [pygame.K_UP]:
-        if box2.y >0:
-            box2.y -= 1
-
-
-    # odbicia pilki
-
-    # sufit i podloga
-    if ball.y +ball.height > rez[1]:
-        rpion=-1
-    if ball.y < 0:
-        rpion=1
-
-    # paletka 2
-    if box2.x <= ball.x + ball.width:
-        if ball.y < box2.y + box2.height and ball.y + ball.height > box2.y:
-            rpoz=-1
-            speed += 5
-
-    # paletka 1
-    if box1.x + box1.width >= ball.x:
-        if ball.y < box1.y + box1.height and ball.y + ball.height > box1.y:
-            rpoz=1
-            speed+=5
+        if pygame.key.get_pressed() [pygame.K_DOWN]:
+            if box2.y + box2.height < rez[1]:
+                box2.y += 4
+        if pygame.key.get_pressed() [pygame.K_UP]:
+            if box2.y >0:
+                box2.y -= 4
 
 
-    # pilka movement
-    if op >= 200:
+        # odbicia pilki
+
+        # sufit i podloga
+        if ball.y +ball.height > rez[1]:
+            rpion= -b_spin
+            if rpoz < 0:
+                rpoz = -b_spin
+            else:
+                rpoz = b_spin
+        if ball.y < 0:
+            rpion= b_spin
+            if rpoz < 0:
+                rpoz = -b_spin
+            else:
+                rpoz = b_spin
+
+        # paletka 2
+        if box2.x <= ball.x + ball.width:
+            if ball.y < box2.y + box2.height and ball.y + ball.height > box2.y:
+                rpoz= -b_spin
+
+        # paletka 1
+        if box1.x + box1.width >= ball.x:
+            if ball.y < box1.y + box1.height and ball.y + ball.height > box1.y:
+                rpoz= b_spin
+
+
+        # pilka movement
         ball.x +=rpoz
         ball.y +=rpion
-        op=0
+        if b_spin < 10:
+            odb += 1
+            if odb > 600:
+                b_spin += 1
+                odb = 0
+        delta = 0
 
-    else:
-        op +=speed
+
 
     if ball.x < 0 or ball.x + ball.width > 1280:
         break
